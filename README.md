36bcac1447fed04816fa042e81109746fba43861
HippErger <berger.clayton@gmail.com>
2020-02-27 12:48:34 -0600

    added screenshot and instructions with clear Todos

---
+++ README.md
@@ -0,0 +1,20 @@
+# TicTacToe-101
+
+## Instructions
+
+1. Fork and clone the [TicTacToe-101 Repo](https://github.com/AustinCodingAcademy/calculator-app-101)
+1. Use Live-Server to open this folder in the browser.
+1. Open your Chrome inspector tool and select the Console tab.
+1. Deselect the "Select an element in the page to inspect" tool - *so you don't have highlights chasing your cursor*.
+1. Click on the top-left box...what happens in the console.
+    * you should see something like the following screenshot:
+    * ![Console-Tab-Example](./images/Console-Tab-Example.png)
+1. Notice the notes on the far right: `scripts.js: 20`, `scripts.js: 43`, and `scripts.js: 44`. These tell you what line of code this log was generated.
+1. Go find the lines these logs came from in the `scripts.js` file.
+1. Go to the `index.html` file and find line 22: `<td id='top-left' onclick="handleClick(this)" ></td>`
+    * The `onclick` method calls the `handleClick()` function in the `scripts.js` file and passes itself, the `<td>` element to it.
+    * Don't move on until you understand how that works.
+1. Now read each of the green comment lines to start piecing together how this app works.
+1. After you have a general understanding find the `// @TODO` comments and fix the problems described.
+1. Go to the HTML file and create all the rows you need for a complete Tic Tac Toe board
+1. When you've finished create a Pull Request(*PR*) on the original repo and turn in the URL of that PR.
--- scripts.js
+++ scripts.js
@@ -2,39 +2,68 @@
 //            INSTRUCTIONS
 //       ***********************
 
-// 1. Read the code below and figure out the data flow
-// 2. Look for the @TODOs, to fix
-// next to each @TODO you will find tasks that need to be finished
-// 4. GET THIS GAME WORKING!!
+// 1. Read the code below one block at a time.
+// 2. Look for the @TODOs, and figure out how to fix them.
+    // next to each @TODO you will find tasks that need to be finished
 
+// The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
 
-// the function that's called when a box is clicked, here "element" will be the same as "this" is in the HTML
+
+
+
+// this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
+// "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
+
+  // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
+  console.log(`The element you clicked on has an id:  ${element.id}`)
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
   if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
-    changeMarker()
   }
 }
 
-// this function places the "currentMarker" inside the HTML element that was clicked.
+
+
+
+
+
+
+
+
+
+
+// this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
+
+  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
+  console.log(`*** The current marker is:  ${currentMarker}. ***`)
+  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
-  document.getElementById(id).innerHTML = currentMarker // comment out this line @CLAYTON
-  console.log(`We'll place a mark on square: ${id}`)
-  // @TODO-1 Set the innerHTML property of the element that was clicked to the "currentMarker"
+  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
   
-  // You will need the following pieces:
+  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
   // document
   // .innerHTML 
+
+  changeMarker()
 }
 
-// This function changes "X" to "O" in the "currentMarker" variable
+
+
+
+
+
+
+
+
+
+// This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
   if(currentMarker === "X"){
     currentMarker = "O"
@@ -43,75 +72,35 @@ const changeMarker = () => {
   }
 }
 
+
+
+
+
+
+
+
+
+
+// This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  // collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-  const squares = document.getElementsByTagName("TD")
+  
+  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
+      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
+    
+  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
+  // squares
+  // .getElementsByTagName("TD")
+  // =
+  // document
+  // const
   
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
-    console.log(squares[i])
+
+    // will log out the id of each square as it loops over them.
+    console.log(squares[i].id)
+
+    // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
-}
-
-
-
-
-
-
-
-
-// We'll use all of the following later in this course
-
-// let board = [
-//   ['','',''],
-//   ['','',''],
-//   ['','','']
-// ];
-
-// ********* We'll use this function later
-// passes the element's id attribute from HTML to be used
-const updateBoard = (id) => {
-  // parses the id string into a number then captures the first and last part the newly create number as row & column
-  const row = parseInt(id.charAt(0))
-  const column = parseInt(id.charAt(2)) 
-
-  console.log(`you clicked the sq at ${row} and ${column}`)
-  console.log(board)
-
-  // @TODO, Your code here: use the above information to change the board variable(array of arrays)
-  // HINT: in your browser open up the dev tools -> console
-}
-
-// ********* We'll use this function later
-const checkForWin = () => {
-  // calls each checkForWin possibility and if any are true gives a page alert,
-  if(horizontalWin() || verticalWin() || diagonalWin()) {
-    // **BONUS** you could make the dismissal of this alert window reset the board...
-    window.alert(`Player ${currentMarker} won!`)
-  } else {
-    // if no win, change the marker from X to O, or O to X for the next player.
-    changeMarker()
-  }
-}
-
-// ********* We'll use this function later
-const horizontalWin = () => {
-  // @TODO, Your code here: to check for horizontal wins
-}
-
-// ********* We'll use this function later
-const verticalWin = () => {
-  // @TODO, Your code here: to check for vertical wins
-}
-
-// ********* We'll use this function later
-const diagonalWin = () => {
-  // @TODO, Your code here: to check for diagonal wins
-}
-
-// ********* We'll use this function later
-const changeMarker = () => {
-  // ternary operator: if it's an X make it an O, if O make it an X
-  currentMarker = currentMarker === "X" ? "O" : "X"
 }
