1f36a530c79cec822437decba6bef534f7c3acfe
Sean <contact@seanyager.com>
2020-03-07 12:04:15 -0600

    TODO complete

--- scripts.js
+++ scripts.js
@@ -44,6 +44,7 @@ const addMarker = (id) => {
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
+  document.getElementById(id).innerHTML = currentMarker;
   
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
@@ -94,6 +95,8 @@ const resetBoard = () => {
   // document
   // const
   
+  const squares = document.getElementsByTagName("TD");
+
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
 
