8f71c36dffb5eb267b4fc1778331650ec0c58ec9
monica-suarez <shutterbugmo@gmail.com>
2020-03-27 12:45:15 -0500

    trying bug fixes

--- scripts.js
+++ scripts.js
@@ -11,15 +11,14 @@ let currentMarker = 'X'
 const handleClick = (element) => {
   console.log(`The element you clicked on has an id:  ${element.id}`)
   if(!document.getElementById(element.id).innerHTML){
-    addMarker(element.id)
+    addMarker(element.id); 
   }
 }
 
-
 const addMarker = (id) => {
-  board[row][column] = currentMarker
   const row = parseInt(element.id.charAt(0))
   const column = parseInt(element.id.charAt(2))
+  board[row][column] = currentMarker
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   console.log(id);
@@ -27,9 +26,9 @@ const addMarker = (id) => {
 }
   const checkForWin = () => {
     if(horizontalWin() || verticalWin() || diagonalWin()) {
-      window.alert(`Player ${currentMarker} won!`)
+      window.alert(`Player ${currentMarker} won!`);
     } else {
-      changeMarker()
+      changeMarker();
     }
   }
 
@@ -98,23 +97,8 @@ const changeMarker = () => {
 const resetBoard = () => {
   console.log()
   const squares = document.getElementsByTagName("td");
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
-  
-  // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
-
-    // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
-
-    // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }
 }
