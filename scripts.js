e4550d96c034158513e34e1c1dcc1c3085e3b9fa
juan-rod <juan.j.rodriguez2@gmail.com>
2020-04-08 20:02:04 -0500

    pushing tictactoe

--- scripts.js
+++ scripts.js
@@ -15,15 +15,17 @@ let currentMarker = 'X'
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
+  // console.log('element:', element)
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
+  // console.log(`The element you clicked on has an id:  ${element.id}`)
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(element.id).innerHTML){
+  if(!document.getElementById(element.id).innerHTML) {
     addMarker(element.id)
   }
+
 }
 
 
@@ -41,10 +43,10 @@ const addMarker = (id) => {
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
-  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
+  // console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+    document.getElementById(id).innerHTML = currentMarker
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
@@ -65,6 +67,7 @@ const addMarker = (id) => {
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
+  console.log('in CHANGEMARKER', currentMarker)
   if(currentMarker === "X"){
     currentMarker = "O"
   } else {
@@ -95,6 +98,10 @@ const resetBoard = () => {
   // const
   
   // loops over the HTML Collection of TDs and clears out the Xs and Os
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
   for (i=0; i < squares.length; i++) {
 
     // will log out the id of each square as it loops over them.
