718c9d79e9d2a0444d3f970a77dfca332dca564a
Devinhodge <d.hodge8891@yahoo.com>
2020-04-24 12:20:44 -0500

    TTT logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 
 let currentMarker = 'X'
+let board = [
+['','',''],
+['','',''],
+['','','']]
 
 const handleClick = (element) => {
   console.log(`The element you clicked on has an id:  ${element.id}`)
@@ -8,12 +12,16 @@ const handleClick = (element) => {
   } 
 }
 const addMarker = (id) => {
+  let index = id.split('-')
+  let row = index[0]
+  let column = index [1]
+  board[row][column] = currentMarker
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
 
 document.getElementById(id).innerHTML = currentMarker
 
-  changeMarker()
+  checkForWin()
 }
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -34,4 +42,48 @@ const resetBoard = () => {
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
+}
+const checkForWin = () => {
+  console.log(board)
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+  // Your code here to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
+    return true
+  }
+  if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")){
+    return true
+  }
+  if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")){
+    return true
+  }
+}
+
+const verticalWin = () => {
+  // Your code here to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
+    return true
+  }
+  if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")){
+    return true
+  }
+  if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")){
+    return true
+  }
+}
+
+const diagonalWin = () => {
+  // Your code here to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")){
+    return true
+  }
+  if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")){
+    return true
+  }
 }
