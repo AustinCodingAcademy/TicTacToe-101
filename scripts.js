acf323a41412696224c708e9e17b1b861332bf90
DavidCastillo4 <David.Castillo4@gmail.com>
2020-04-24 17:47:30 -0500

    TTT finished

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 
 let currentMarker = 'X'
-let a={};
-let n=1
+let a = {};
+let n = 1
 
-while (n <= 9)
-{
-a[n]="";
-n++
+while (n <= 9) {
+  a[n] = "";
+  n++
 }
 
 const handleClick = (element) => {
@@ -17,65 +16,64 @@ const handleClick = (element) => {
 
 const addMarker = (id) => {
   document.getElementById(id).innerHTML = currentMarker;
-  a[id]=currentMarker    
+  a[id] = currentMarker
   changeMarker()
- 
 }
 
-const changeMarker = () => {
+
+let changeMarker = () => {
   if (currentMarker === "X") {
     currentMarker = "O"
   } else {
-    currentMarker = "X"  
+    currentMarker = "X"
   }
-  
-  fn();
+
+  CheckForWinner();
 }
 
-const resetBoard = () => {  
+const resetBoard = () => {
   let squares = document.getElementsByTagName('TD')
   for (i = 0; i < squares.length; i++) {
     console.log(squares[i].id)
     squares[i].innerHTML = null
   }
-  fnResetArr()
+  resetArray()
 }
 
 
-let h1,h2,h3,v1,v2,v3,d1,d2
+let h1, h2, h3, v1, v2, v3, d1, d2
 
-let fn = () => {
-  h1=a[1]+a[2]+a[3]
-  h2=a[4]+a[5]+a[6]
-  h3=a[7]+a[8]+a[9]
+let CheckForWinner = () => {
+  h1 = a[1] + a[2] + a[3]
+  h2 = a[4] + a[5] + a[6]
+  h3 = a[7] + a[8] + a[9]
 
-  v1=a[1]+a[4]+a[7]
-  v2=a[2]+a[5]+a[8]
-  v3=a[3]+a[6]+a[9]
+  v1 = a[1] + a[4] + a[7]
+  v2 = a[2] + a[5] + a[8]
+  v3 = a[3] + a[6] + a[9]
 
-  d1=a[1]+a[5]+a[9]
-  d2=a[3]+a[5]+a[7]
+  d1 = a[1] + a[5] + a[9]
+  d2 = a[3] + a[5] + a[7]
 
-  hvd=[h1,h2,h3,v1,v2,v3,d1,d2]
+  hvd = [h1, h2, h3, v1, v2, v3, d1, d2]
 
   hvd.forEach(e => {
-    if (e=="XXX" || e == "OOO" )
-    {
-    console.log('winner')  
-    fnResetArr()    
-    alert('Winner')
-  }
-})
+    if (e == "XXX" || e == "OOO") {
+      console.log('winner')
+      setTimeout(() => {
+        alert('Winner Winner Chicken Dinner!!!!')
+      }, 100);
+    }
+  })
 }
 
 
-let fnResetArr=()=> {
-  h1='',h2='',h3='',v1='',v2='',v3='',d1='',d2=''
-  for (let indexVal in a) {    
-    a[indexVal]="";
+let resetArray = () => {
+  h1 = '', h2 = '', h3 = '', v1 = '', v2 = '', v3 = '', d1 = '', d2 = ''
+  for (let indexVal in a) {
+    a[indexVal] = "";
   };
-  
-  //console.log(a)
+
 }
 
 
