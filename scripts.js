c3404ca1e6738757a1a8c39765eac68ff7ac43f5
JEFFGULICK <jeffgulick@gmail.com>
2020-05-13 20:48:35 -0500

    fixed error

--- scripts.js
+++ scripts.js
@@ -91,7 +91,7 @@ const resetBoard = () => {
    const diagonalWin = () => {
     if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")){
       return true;
-    } else if((board[0][2] == "X" && board[1][1] == "X" && board[0][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[0][0] == "O")){
+    } else if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")){
       return true;
     }else {
       return false;
