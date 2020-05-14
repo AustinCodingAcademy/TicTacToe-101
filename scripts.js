cb8b440382e36ed68fc13a4b2a57d3089b514ad8
JEFFGULICK <jeffgulick@gmail.com>
2020-05-13 20:23:50 -0500

    added logic to ttt

--- scripts.js
+++ scripts.js
@@ -71,13 +71,29 @@ const resetBoard = () => {
       return true;
     }else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[2][1] == "O" && board[2][2] == "O")){
       return true;
-    } 
+    }else{
+      return false;
+    }
    }
 
    const verticalWin = () => {
-     // Your code here to check for vertical wins
+    if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
+      return true;
+    } else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")){
+      return true;
+    }else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[2][1] == "O" && board[2][2] == "O")){
+      return true;
+    }else {
+      return false;
+    } 
    }
 
    const diagonalWin = () => {
-     // Your code here to check for diagonal wins
-   }
+    if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")){
+      return true;
+    } else if((board[0][2] == "X" && board[1][1] == "X" && board[0][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[0][0] == "O")){
+      return true;
+    }else {
+      return false;
+    }
+    } 
