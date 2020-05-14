c05d818d8540be4b588be88731e6bc6e46403f3e
JEFFGULICK <jeffgulick@gmail.com>
2020-05-13 19:35:56 -0500

    added logic to game

--- scripts.js
+++ scripts.js
@@ -1,36 +1,36 @@
-//       ***********************
-//            INSTRUCTIONS
-//       ***********************
-
-// 1. Read the code below one block at a time.
-// 2. Look for the @TODOs, and figure out how to fix them.
-    // next to each @TODO you will find tasks that need to be finished
-
+//array to hold selections
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
-const handleClick = (fork) => {
+const handleClick = (element) => {
 
-  console.log(`The element you clicked on has an id:  ${fork.id}`)
-
-  // this next line prevents an X being changed to an O or an O being changed to an X by...
+  console.log(`The element you clicked on has an id:  ${element.id}`)
   //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(fork.id).innerHTML){
-
-    addMarker(fork.id)
-
+  if(!document.getElementById(element.id).innerHTML){
+    addMarker(element.id)
   }
+
 }
+
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
-  document.getElementById(id).innerHTML = currentMarker // still trying to figure out exactly what this does. Have an idea, but not sure
+  document.getElementById(id).innerHTML = currentMarker 
+  //changing id's into integers
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+  board[row][column] = currentMarker //assigning a postion to 'x' or 'o'
 
-  changeMarker() //calls the function below
+  checkForWin() //checks for winner
 }
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
@@ -49,31 +49,35 @@ const resetBoard = () => {
     
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
-
     // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
-
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
 }
 
-const checkForWin = () => {
-  if(horizontalWin() || verticalWin() || diagonalWin()) {
-    window.alert(`Player ${currentMarker} won!`)
-  } else {
-    changeMarker()
-  }
-}
+ const checkForWin = () => {
+     if(horizontalWin() || verticalWin() || diagonalWin()) {
+       window.alert(`Player ${currentMarker} won!`)
+     } else {
+       changeMarker()
+     }
+   }
 
-const horizontalWin = () => {
-  // Your code here to check for horizontal wins
-}
+   const horizontalWin = () => {
+    if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
+      return true;
+    } else if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")){
+      return true;
+    }else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[2][1] == "O" && board[2][2] == "O")){
+      return true;
+    } 
+   }
 
-const verticalWin = () => {
-  // Your code here to check for vertical wins
-}
+   const verticalWin = () => {
+     // Your code here to check for vertical wins
+   }
 
-const diagonalWin = () => {
-  // Your code here to check for diagonal wins
-}
+   const diagonalWin = () => {
+     // Your code here to check for diagonal wins
+   }
