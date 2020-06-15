6263d08370eb6060f2edd0e54fd139f8deeec5dc
michaelscoggins <rhythmghosts@gmail.com>
2020-06-15 15:23:54 -0500

    first commit

--- scripts.js
+++ scripts.js
@@ -44,12 +44,14 @@ const addMarker = (id) => {
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  document.getElementById(id).innerHTML= currentMarker
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
   // document
   // .innerHTML 
+
+  
 
   changeMarker()
 }
@@ -85,8 +87,8 @@ const changeMarker = () => {
 const resetBoard = () => {
   
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
+  const squares = document.getElementsByTagName("TD")
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
   // .getElementsByTagName("TD")
