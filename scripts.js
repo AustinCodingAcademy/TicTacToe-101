0056cbe7a48929d5f0f4e360f654186879b80ec5
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-06-15 16:32:41 -0500

    finished ttt

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ let currentMarker = 'X'
 const handleClick = (element) => {
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
+  console.log(`!!!The element you clicked on has an id:  ${element}`)
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
@@ -44,7 +44,7 @@ const addMarker = (id) => {
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  document.getElementById(id).innerHTML = currentMarker ;
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
@@ -86,7 +86,7 @@ const resetBoard = () => {
   
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
+const squares = document.getElementsByTagName ("TD") 
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
   // .getElementsByTagName("TD")
@@ -101,6 +101,6 @@ const resetBoard = () => {
     console.log(squares[i].id)
 
     // sets the innerHTML to null to replace the "X" or "O"
-    squares[i].innerHTML = null
+  squares[i].innerHTML = null
   }  
 }
