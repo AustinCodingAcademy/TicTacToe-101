52fec9b6faf8156df7f5208f7618e798859c306f
michaelscoggins <rhythmghosts@gmail.com>
2020-06-24 08:04:46 -0500

    morning commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+// let row = [
+//   [[0][0],[0][1],[0][2]],
+//   [[1][0],[1][1],[1][2]],
+//   [[2][0],[2][1],[2][2]],  
+// ]
+
+// let column = [
+//   [[0][0],[1][0],[2][0],
+//   []
+
+//   ]
+// ]
+
 //       ***********************
 //            INSTRUCTIONS
 //       ***********************
@@ -7,14 +20,19 @@
     // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
+
 let currentMarker = 'X'
-
-
-
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
+
 const handleClick = (element) => {
+  
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
@@ -27,18 +45,15 @@ const handleClick = (element) => {
 }
 
 
-
-
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+
+  board[row][column] = currentMarker
+
+  console.log(board)
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
@@ -49,21 +64,11 @@ const addMarker = (id) => {
   // = currentMarker
   // .getElementById(id)
   // document
-  // .innerHTML 
+  // .innerHTML  
 
-  
+  checkForWin()
 
-  changeMarker()
 }
-
-
-
-
-
-
-
-
-
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -74,17 +79,8 @@ const changeMarker = () => {
   }
 }
 
-
-
-
-
-
-
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
-const resetBoard = () => {
+const resetBoard = () => {  
   
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
   const squares = document.getElementsByTagName("TD")
@@ -104,5 +100,60 @@ const resetBoard = () => {
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
-  }  
+  } 
 }
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`) 
+  } else {
+      changeMarker()
+    }  
+  console.log(checkForWin)
+}
+
+
+const horizontalWin = () => {
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")) {
+    return true
+  } else if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")) {
+      return true
+    } else if ((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) {
+        return true
+      } else {
+          return false
+        }
+}
+
+const verticalWin = () => {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")) {    
+    return true
+  } else if ((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")) {      
+      return true
+    } else if ((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")) {
+        return true
+      } else {
+          return false
+        }
+
+}
+
+const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")) {
+    return true
+  } else if ((board[2][0] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[2][0] == "O" && board[1][1] == "O" && board[2][0] == "O")) {
+      return true
+    } else {
+        return false
+      }
+}
+
+  
+
+
+
+
+
+
+
+
