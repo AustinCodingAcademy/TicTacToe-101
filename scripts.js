7d34c7cb46be531f7c92e3c7120ff60ce8204e63
Cody-Blair <cb.rialbc@gmail.com>
2020-08-02 17:00:48 -0600

    finished TTT

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ let currentMarker = 'X'
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => { 
-  let hasMarkerInSquare = document.getElementById(element.id).innerHTML = currentMarker
+  let hasMarkerInSquare = document.getElementById(element.id).innerHTML
     
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
@@ -23,7 +23,7 @@ const handleClick = (element) => {
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(element.id).innerHTML){
+  if(!hasMarkerInSquare){
     addMarker(element.id)
   }
 }
@@ -46,14 +46,14 @@ const addMarker = (id) => {
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  document.getElementById(id).innerHTML = currentMarker
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
   // document
   // .innerHTML 
 
-  changeMarker(document.getElementById(id).innerHTML) = currentMarker
+  changeMarker()
 }
 
 
