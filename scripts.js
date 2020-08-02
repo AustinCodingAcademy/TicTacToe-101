a392d1ccfe378f7602d0de5b3ab10624918286b2
AustinBeal <austinbeal89@gmail.com>
2020-08-02 16:55:07 -0500

    added line to make resrt button work, finished!

--- scripts.js
+++ scripts.js
@@ -57,7 +57,7 @@ const resetBoard = () => {
  
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-     
+     const squares = document.getElementsByTagName("TD")
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
   // .getElementsByTagName("TD")
