c8da85a32906299c4b72b35aab533ffc76513d53
RacMcT <raccmct@gmail.com>
2021-01-23 10:41:20 -0600

    rules for winning in js

--- scripts.js
+++ scripts.js
@@ -112,10 +112,19 @@ const verticalWin = () => {
   // Your code here to check for vertical wins
   if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || 
   (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O"))
-
   
+  ((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || 
+  (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O"))
+  
+  ((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || 
+  (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"))
 }
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || 
+  (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O"))
+
+  ((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || 
+  (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O"))
 }
