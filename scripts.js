9eedd57ec5ee906d8a8391437077f0efd0d2c08d
pjwhite08 <pamelajeanwhite@gmail.com>
2021-05-21 17:03:55 -0500

    working, not styled

--- scripts.js
+++ scripts.js
@@ -5,15 +5,11 @@
 // 1. Read the code below one block at a time.
 // 2. Look for the @TODOs, and figure out how to fix them.
 // next to each @TODO you will find tasks that need to be finished
-
-// The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
-let currentMarker = 'O'
-
-
-
-
-// this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
-// "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
 const handleClick = (element) => {
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
@@ -26,19 +22,69 @@ const handleClick = (element) => {
   }
 }
 
+//const row = parseInt(element.id.charAt(0))
+//const column = parseInt(element.id.charAt(2))
+//board[row][column] = currentMarker
+
+// The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
+let currentMarker = 'O'
 
 
 
 
+// this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
+// "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 
 
 
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+if((board[0][0] == currentMarker && board[0][1] == currentMarker && board[0][2] == currentMarker) 
+ || (board[1][0] == currentMarker && board[1][1] == currentMarker && board[1][2] == currentMarker) 
+|| (board[2][0] == currentMarker && board[2][1] == currentMarker && board[2][2] == currentMarker))
+{return true}
+}
 
 
+ const verticalWin = () => {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+|| (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+ || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+ || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+ || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+ || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"))
+ {return true}
+ }
+//what "then" goes here??
+
+
+const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  || (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O"))
+  {return true}
+}
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
-const addMarker = (id) => {
 
+const addMarker = (id) => {
+  console.log(id)
+  let element = document.getElementById(id)
+  console.log(element)
+  const row = parseInt(element.id.charAt(0))
+const column = parseInt(element.id.charAt(2))
+board[row][column] = currentMarker
+console.log(board)
+console.log(row)
+console.log(column)
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
@@ -50,11 +96,9 @@ const addMarker = (id) => {
   // .getElementById(id)
   // document
   // .innerHTML 
-
-  changeMarker()
+  
+  checkForWin()
 }
-
-
 
 
 
@@ -108,4 +152,8 @@ const resetBoard = () => {
 
 
 console.log (document.getElementsByTagName("h1"));
+
+
+
+
 
