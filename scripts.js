4aff09ce7b7ccafac428dadd94c470cf6c57f2c6
MichaelABartlett <michael@machinistchoice.com>
2021-05-23 20:06:04 -0500

    save changes could not get win to be picked up

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@
 //       ***********************
 
 
-let board = [["","",""],
+let board = [
             ["","",""],
-            ["","",""]]
+            ["","",""],
+            ["","",""]
+          ]
 
           
 // 1. Read the code below one block at a time.
@@ -13,7 +15,9 @@ let board = [["","",""],
     // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
-let currentMarker = 'X'
+
+
+
 
 
 
@@ -21,8 +25,9 @@ let currentMarker = 'X'
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
+  
   console.log('element:', element)
-
+  
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
 
@@ -32,6 +37,13 @@ const handleClick = (element) => {
     addMarker(element.id)
   }
 }
+
+
+
+
+
+
+let currentMarker = 'X'
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 //let element = document.getElementById(id)
@@ -60,9 +72,10 @@ const addMarker = (id) => {
   // .getElementById(id)
   // document
   // .innerHTML 
-  document.getElementById(thisIsId).innerHTML = currentMarker
+  document.getElementById(id).innerHTML = currentMarker
 
   changeMarker()
+
 }
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
@@ -86,7 +99,7 @@ const resetBoard = () => {
   // =
   // document
   // const
-  const squares = document.getElementsByTagName("TD")
+  const squares = document.getElementsByTagName("td")
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
 
@@ -108,37 +121,50 @@ const checkForWin = () => {
 
 
 
-// const horizontalWin = () => {
-// if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
-// || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
-// )
-// if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
-// || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
-// )
-// if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
-// || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
-// )
-// }
+const horizontalWin = () => {
+if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+|| (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")){
+  alert(currentMarker);
+  }
+
+else if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+|| (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")){
+  alert(currentMarker + " wins");
+  }
+
+else((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+|| (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O"));{
+  alert(currentMarker + " wins");
+  }
+}
 
 // const verticalWin = () => {
 // if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-// || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-// )
-// if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
-// || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
-// )
-// if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
-// || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
-// )
+// || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")){
+//   alert(currentMarker + " wins");
+//   }
+
+// else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+// || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")){
+//   alert(currentMarker + " wins");
+//   }
+
+// else if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+// || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")){
+//   alert(currentMarker + " wins");
+//   }
 // }
 
 // const diagonalWin = () => {
 // if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
-// || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
-// )
-// if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
-// || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
-// )
+// || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")){
+//   alert(currentMarker + " wins");
+//   }
+
+// else if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+// || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")){
+//   alert(currentMarker + " wins");
+//   }
 // }
 
 
