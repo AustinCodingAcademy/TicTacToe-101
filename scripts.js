fa4173e2db2590c37d3770e04ff1fd01a0d21305
MHargrove96 <michaelh.96@yahoo.com>
2021-11-02 21:29:31 -0500

    tictactoe update

--- scripts.js
+++ scripts.js
@@ -8,14 +8,18 @@
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
-
-
-
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
+// const row = parseInt(element.id.charAt(0))
+// const column = parseInt(element.id.charAt(0))
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
-const handleClick = (element) => {
 
+const handleClick = (element) => {
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
 
@@ -29,39 +33,18 @@ const handleClick = (element) => {
 
 
 
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
-
-  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
+  document.getElementById(id).innerHTML = currentMarker
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+  // board[row][column] = currentMarker
   
-  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-
-  document.getElementById(id) .innerHTML = currentMarker
-
   
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
-
-  changeMarker()
+  checkForWin()
 }
-
-
-
-
-
-
 
 
 
@@ -76,11 +59,45 @@ const changeMarker = () => {
 }
 
 
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+      console.log("checkForWin was false")
+    changeMarker()
+  } 
+}
 
+const horizontalWin = () => {
+  if((board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+  || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+  || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) {
+    return true
+  }
+}
 
+const verticalWin = () => {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+  || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+  || (board[0][3] == "X" && board[1][3] == "X" && board[2][3] == "X") 
+  || (board[0][3] == "O" && board[1][3] == "O" && board[2][3] == "O")) {
+   return true
+ }
+}
 
-
-
+const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  || (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")) {
+    return true
+  }
+}
 
 
 
