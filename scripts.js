bc43ca43f14b689a39f908aa31ca151cc083cc7f
MHargrove96 <michaelh.96@yahoo.com>
2021-11-03 20:03:25 -0500

    finished win alert

--- scripts.js
+++ scripts.js
@@ -40,7 +40,7 @@ const addMarker = (id) => {
   document.getElementById(id).innerHTML = currentMarker
   const row = parseInt(id.charAt(0))
   const column = parseInt(id.charAt(2))
-  // board[row][column] = currentMarker
+  board[row][column] = currentMarker
   
   
   checkForWin()
@@ -69,7 +69,7 @@ const checkForWin = () => {
 }
 
 const horizontalWin = () => {
-  if((board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X") 
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
   || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
   || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
   || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
