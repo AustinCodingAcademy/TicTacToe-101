b2a646470c3d7092f4151d9da1362d853d78b0e0
jpswt <joel_swt@yahoo.com>
2022-02-03 21:21:24 -0600

    fix typo in script.js

--- scripts.js
+++ scripts.js
@@ -89,15 +89,7 @@ const changeMarker = () => {
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
   
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
+
 
   const squares = document.getElementsByTagName("TD")
   
@@ -109,8 +101,9 @@ const resetBoard = () => {
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
+
+    window.location.reload()
   }
-  window.location.reload()
 }
 
 
