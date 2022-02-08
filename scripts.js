6944341f163360977071a6ba34be0eec3185d6a9
RobertCardoso <robert.rcardoso@hotmail.com>
2022-02-07 22:36:23 -0600

    not working

--- scripts.js
+++ scripts.js
@@ -30,6 +30,7 @@ const handleClick = (element) => {
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
+  
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
@@ -43,9 +44,15 @@ const addMarker = (id) => {
   // .innerHTML 
 document.getElementById(id).innerHTML = currentMarker 
 
-  changeMarker()
-}
+let board = [
+  ["", "", ""]
+  ["", "", ""]
+  ["", "", ""]
+]
 
+ checkForWin()
+}
+// checkForWin()
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -57,6 +64,55 @@ const changeMarker = () => {
 }
 
 
+
+
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+const horizontalWin = () => {
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+        || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O"))
+  else if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O"))
+
+  else if ((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O"))
+  {
+    return true;
+  }
+        
+        
+}
+
+const verticalWin = () => {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+        || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") )  
+        else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+        || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O"))
+        else if ((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+        || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O"))
+        {
+          return true;
+        }
+    
+}
+
+const diagonalWin = () => {
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+        || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+    )
+    else ((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+    || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O"))
+    {
+      return true;
+    }
+}
 
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
@@ -71,7 +127,6 @@ const resetBoard = () => {
   // =
   // document
   // const
-  
   const squares = document.getElementsByTagName("td")
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
@@ -83,3 +138,18 @@ const resetBoard = () => {
     squares[i].innerHTML = null
   }  
 }
+
+
+
+//      if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+//      || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") 
+// )
+
+//     if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+//          || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+//      )  ;
+
+
+//     if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+//     || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+// )
