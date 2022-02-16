aa2e37b30e5f271d3408f1d532813dd137cd7597
mblester <morganmurry@gmail.com>
2022-02-15 20:31:52 -0600

    tic tac toe first

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@
 //            INSTRUCTIONS
 //       ***********************
 
+let jumbotron = document.getElementsByClassName("jumbotron")
 // 1. Read the code below one block at a time.
 // 2. Look for the @TODOs, and figure out how to fix them.
     // next to each @TODO you will find tasks that need to be finished
@@ -44,7 +45,7 @@ const addMarker = (id) => {
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  let element = document.getElementById(id).innerHTML = currentMarker;
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
