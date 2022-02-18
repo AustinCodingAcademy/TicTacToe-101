50a1206d91d99d557b6d340513e79ab50933466a
mblester <morganmurry@gmail.com>
2022-02-17 19:02:16 -0600

    reset button

--- scripts.js
+++ scripts.js
@@ -56,14 +56,6 @@ const addMarker = (id) => {
 }
 
 
-
-
-
-
-
-
-var squares ;
-
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
   if(currentMarker === "X"){
@@ -82,33 +74,26 @@ const changeMarker = () => {
 
 
 
-// This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-document.getElementsByTagName("td");
-document.write(x.length);
 
-}
-
-  
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-      //const x = document.getElementsByTagName("td");  
-      //document.write(x.length);
+  // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
+
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
   // .getElementsByTagName("TD")
   // =
   // document
   // const
-  //const squares = document.getElementsByTagName("td");
-  //document(x.length)
-  
+  const squares = document.getElementsByTagName("TD");
+
   // loops over the HTML Collection of TDs and clears out the Xs and Os
-  for (i=0; i < squares.length; i++) {
+  for (i = 0; i < squares.length; i++) {
 
     // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
-  }  
+  }
+}
