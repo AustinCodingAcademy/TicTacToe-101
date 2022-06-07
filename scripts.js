a0f24bfe227f52cbc65c78374e010cf711b87d04
jtylorlane <jtylorlane@yahoo.com>
2022-06-07 12:11:28 -0500

    tictacttoe

--- scripts.js
+++ scripts.js
@@ -51,6 +51,7 @@ const addMarker = (id) => {
   // document
   // .innerHTML 
 
+  document. getElementById(id).innerHTML = currentMarker
   changeMarker()
 }
 
@@ -93,6 +94,8 @@ const resetBoard = () => {
   // =
   // document
   // const
+
+  const squares = document.getElementsByTagName("TD");
   
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
