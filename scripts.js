3000e240256821e1d96990552b23c759ff82ecf8
j3553pat <jessecodedev@gmail.com>
2022-11-28 18:38:38 -0600

    fixed reset button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let board = [
-  ["", "", ""], // <-- Row 1, index 0
-  ["", "", ""], // <-- Row 2, index 1
-  ["", "", ""] // <-- Row 3, index 2
+  ["", "", ""],
+  ["", "", ""], 
+  ["", "", ""] 
 ]
 //       ***********************
 //            INSTRUCTIONS
@@ -62,21 +62,19 @@ const changeMarker = () => {
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
   
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
+
     const squares = document.getElementsByTagName("td")
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
-  
-  // loops over the HTML Collection of TDs and clears out the Xs and Os
+
   for (i=0; i < squares.length; i++) {
 
     // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
+
+    board = [
+      ["", "", ""],
+      ["", "", ""], 
+      ["", "", ""] 
+    ]
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
