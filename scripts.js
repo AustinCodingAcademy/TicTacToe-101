4bd56e2b816a9e793b7903dd810f006398422666
jdgrobeck <jdgrobeck@gmail.com>
2023-01-24 11:32:52 -0600

    adding win logic

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-//       ***********************
-//            INSTRUCTIONS
-//       ***********************
 
-// 1. Read the code below one block at a time.
-// 2. Look for the @TODOs, and figure out how to fix them.
-    // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
+  let board = [
+    ["", "", ""], // <-- Row 1, index 0
+    ["", "", ""], // <-- Row 2, index 1
+    ["", "", ""] // <-- Row 3, index 2
+  ]
+
 
 
 
@@ -27,42 +27,27 @@ const handleClick = (element) => {
 }
 
 
-
-
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
-  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
-  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
+
 
   document.getElementById(id).innerHTML = currentMarker;
 
-  changeMarker()
+  const row = parseInt(id.charAt(0));
+
+  const column = parseInt(id.charAt(2));
+
+  board [row][column] = currentMarker;
+
+  // changeMarker()
+  checkForWin()
+  
+  
 }
-
-
-
-
-
-
-
-
 
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
@@ -72,29 +57,13 @@ const changeMarker = () => {
   } else {
     currentMarker = "X"
   }
+  // checkForWin()
 }
-
-
-
-
-
-
-
-
 
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
   
-  // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
-      // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
-  // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // squares
-  // .getElementsByTagName("TD")
-  // =
-  // document
-  // const
   const squares = document.getElementsByTagName("TD"); 
   
 
@@ -107,5 +76,107 @@ const resetBoard = () => {
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
-  currentMarker = 'X'
+
+  location.reload();
+  changeMarker()
+  
+  // currentMarker = 'X'
 }
+
+// Checking for top horizontal win //
+
+const horizontalWin = () => {
+
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+      || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")) {
+      
+      return true;
+  }
+    
+  else if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")) {
+
+    return true;
+  }
+    
+  else if ((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) {
+
+    return true;
+  }
+
+  else {
+    return false;
+  }
+
+}
+
+// Vetical Win //
+
+const verticalWin = () => {
+
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+    || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")) {
+    
+      return true;
+  }
+
+
+  if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+    || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")) {
+    
+      return true;
+  }
+
+
+  if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+    || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")){
+    
+      return true;
+  }
+
+  else {
+    return false;
+  }
+
+}
+
+// Diagonal Win //
+
+const diagonalWin = () => {
+
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+  || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")) {
+  
+    return true;
+}
+
+  if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+  || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")) {
+  
+    return true;
+}
+
+else {
+  return false;
+}
+  
+}
+
+
+checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+    console.log(`Player ${currentMarker} won!`)
+    resetBoard ()
+  } 
+
+  else {
+    changeMarker()}
+  
+  }
+
+
+
+
+
