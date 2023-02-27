0287998919dfa2785e61e77aa5dfacb541ebd57f
ruru-dev <rurutia89@gmail.com>
2023-02-26 23:28:49 -0600

    add winner logic

--- scripts.js
+++ scripts.js
@@ -5,14 +5,12 @@ let board = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""]
-]
-;
+];
+
+let winner;
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
-
-
-
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
@@ -23,37 +21,35 @@ const handleClick = (element) => {
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
-  if(!document.getElementById(element.id).innerHTML){
+  if(!winner && !document.getElementById(element.id).innerHTML){
     addMarker(element.id)
   }
 }
 
-
-
-
-
-
-
-
-
-
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
-  // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
+  // Log the current marker
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
-  // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  document.getElementById(id).innerHTML = currentMarker;
-  // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
+  // Get the square that was clicked
+  const clickedSquare = document.getElementById(id);
 
-  changeMarker()
+  // Determine what row/column the square was on based on its id
+  const row = parseInt(clickedSquare.id.charAt(0));
+  const column = parseInt(clickedSquare.id.charAt(2));
+
+  // Mark it in the data structure for the board
+  board[row][column] = currentMarker;
+
+  // Mark it in the html for the board
+  clickedSquare.innerHTML = currentMarker;
+  
+  // Log the current state of the board
+  console.log(board);
+
+  checkForWin();
 }
 
 
@@ -89,25 +85,67 @@ const resetBoard = () => {
 
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
-  }  
+  }
+
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ];
+  currentMarker = 'X';
+  winner = null;
+
+  console.log(board);
 }
 
-// Set of Rules
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    winner = currentMarker;
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
 
-// Horizontal Win, a.k.a, If row 0 equals all "X"s or "O"s:
+const horizontalWin = () => {
+  // Your code here to check for horizontal wins
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+      || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+      || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+      || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+      || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+      || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+    ){
+      return true;
+    }
 
-if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
-|| (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
-)
+    return false;
+}
 
-// Vertical Wins, a.k.a, if column 0 equals all "X"s or "O"s:
+const verticalWin = () => {
+  // Your code here to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+    || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+    || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+    || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+    || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+    || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+  ){
+    return true;
+  }
 
-if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-|| (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-)
+  return false;
+}
 
-// Diagonal Wins, a.k.a, If there is a line created diagonally across the board with "X"s or with "O"s:
+const diagonalWin = () => {
+  // Your code here to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+    || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+    || (board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+    || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+  ) {
+    return true;
+  }
 
-if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
-|| (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
-)
+  return false;
+}
