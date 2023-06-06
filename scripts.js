963d6196164451115283dd954ac5e2ae9fc642f8
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-06-06 11:52:14 -0500

    Update scripts.js
    got rid of some nonsense at the bottom of my script.

--- scripts.js
+++ scripts.js
@@ -80,9 +80,3 @@ function reverseString(str) {
   return str.split("").reverse().join("");
 }
 console.log(reverseString(`hello world`));
-
-document
-  .getElementsByClassName(".jumbotron")
-  .addEventListener("click", function (element) {
-    console.log(element);
-  });
