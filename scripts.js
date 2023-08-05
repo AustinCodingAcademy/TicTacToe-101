948feb5c6b1a37dea239628bf281c3053ad0ffba
chrisstrom1 <wchrisstrom@yahoo.com>
2023-08-05 13:04:02 -0500

    latest commit1

--- scripts.js
+++ scripts.js
@@ -7,7 +7,11 @@
     // next to each @TODO you will find tasks that need to be finished
 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
-Array("", "", "", )
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""]
+]
 
 let currentMarker = 'X'
 
@@ -16,7 +20,9 @@ let currentMarker = 'X'
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
-const handleClick = (element) => 
+const handleClick = (element) => {
+
+
 
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
@@ -26,33 +32,33 @@ const handleClick = (element) =>
   if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
   }
-
+}
 
 
 
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+
 
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  document.getElementById(id).innerHTML= currentMarker
+board[row][column]= currentMarker
+console.log(board)
+checkForWin()
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
   // document
   // .innerHTML 
-
-  changeMarker()
 }
-
-
-
-
-
+  
 
 
 
@@ -73,9 +79,6 @@ const changeMarker = () => {
 
 
 
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
   
@@ -89,6 +92,7 @@ const resetBoard = () => {
   // document
   // const
   
+  const squares= document.getElementsByTagName("TD")
   // loops over the HTML Collection of TDs and clears out the Xs and Os
   for (i=0; i < squares.length; i++) {
 
@@ -98,4 +102,37 @@ const resetBoard = () => {
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
+  
 }
+
+const checkForWin= ()=>{
+  if (horizontalWin()|| verticleWin()) {
+    window.alert(`Player ${currentMarker} Won`)
+  }
+  else {
+    changeMarker()
+  }
+  
+}
+const horizontalWin= ()=> {
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
+  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
+  || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X")
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+  || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X")
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+){
+ return true
+}
+}
+const verticleWin= ()=> {
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+  || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  || (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
+  || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+  || (board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X")
+  || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+  ){
+    return true
+   }
+   }
